fedab8a9d577af8846bcfaf830904c468393399d
NinaAurora <ninika@gmail.com>
2021-07-28 12:45:32 +0200

    Library toggle and select added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 //Import Util
 import data from "./util";
 
@@ -19,6 +20,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
@@ -26,6 +28,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
         audioRef={audioRef}
@@ -33,14 +36,15 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
         setSongInfo={setSongInfo}
-        songInfo={setSongInfo}
-        setSongs={setSongs}
-        setCurrentSong={setCurrentSong}
+        songInfo={songInfo}
       />
       <Library
+        audioRef={audioRef}
         songs={songs}
-        setCurrentsong={setCurrentSong}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
       <audio
         onTimeUpdate={timeUpdateHandler}
